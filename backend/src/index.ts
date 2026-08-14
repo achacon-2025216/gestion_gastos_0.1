566ea86eaa3e6bb9c7e9c6962c2ece7d3890d1a7
achacon-2025216 <achacon-2025216@kinal.edu.gt>
2026-08-13 22:17:43 -0600

    feat: Se agrego el index.ts para la conexion del servidor

---
+++ backend/src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import authRoutes from './routes/authRoutes.js';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+// Rutas de autenticación
+app.use('/api/auth', authRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
