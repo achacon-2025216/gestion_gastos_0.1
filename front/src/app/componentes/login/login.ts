4a1ae33e783b8d579a751b417d3dc20b8a4546da
achacon-2025216 <achacon-2025216@kinal.edu.gt>
2026-08-14 00:56:51 -0600

    feat: Se cambio el login.html y login.ts por inicio de sesion

--- front/src/app/componentes/login/login.ts
+++ front/src/app/componentes/login/login.ts
@@ -1,13 +1,38 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router'; // <--- 1. Importa RouterLink
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink], // <--- 2. Agrégalo aquí en los imports
+  imports: [CommonModule, FormsModule, RouterLink, HttpClientModule],
   templateUrl: './login.html',
-  styleUrl: './login.css',
+  styleUrl: './login.css'
 })
-export class LoginComponent {
 
+export class LoginComponent {
+  username = '';
+  password = '';
+  errorMessage = '';
+
+  constructor(private http: HttpClient, private router: Router) {}
+
+  onLogin() {
+    const credentials = { username: this.username, password: this.password };
+
+    // Agregado '/api' a la URL
+    this.http.post<any>('http://localhost:3000/api/login', credentials).subscribe({
+      next: (response) => {
+        localStorage.setItem('token', response.token);
+        localStorage.setItem('role', response.role);
+        this.router.navigate(['/inicio-gastos']);
+      },
+      error: (err) => {
+        this.errorMessage = 'Usuario o contraseña incorrectos';
+        console.error(err);
+      }
+    });
+  }
 }
