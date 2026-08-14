809aed068a9d70e8e3fddfa8306c9a9a923ef6c9
achacon-2025216 <achacon-2025216@kinal.edu.gt>
2026-08-14 00:52:53 -0600

    feat: Se agrego un import

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,25 @@
+import 'dotenv/config';
 import express from 'express';
+import cors from 'cors';
 import authRoutes from './routes/authRoutes.js';
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+// Importaciones necesarias para el adaptador de PostgreSQL en Prisma
+import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import pg from 'pg';
 
+// Configurar la conexión con el pool de Postgres y el adaptador
+const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL });
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
+
+const app = express();
+
+app.use(cors());
 app.use(express.json());
 
-// Rutas de autenticación
-app.use('/api/auth', authRoutes);
+app.use('/api', authRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+app.listen(3000, () => {
+  console.log('Servidor corriendo en http://localhost:3000');
 });
