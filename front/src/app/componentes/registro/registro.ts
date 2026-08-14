122a8ef2c686b5059e74379a020c3015f149cdb9
achacon-2025216 <achacon-2025216@kinal.edu.gt>
2026-08-14 00:57:44 -0600

    feat: Se cambio el registro.html y registro.ts para registrar usuario

--- front/src/app/componentes/registro/registro.ts
+++ front/src/app/componentes/registro/registro.ts
@@ -1,9 +1,40 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-registro',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule, RouterLink, HttpClientModule],
   templateUrl: './registro.html',
-  styleUrl: './registro.css',
+  styleUrl: './registro.css'
 })
-export class RegistroComponent {}
+
+export class RegistroComponent {
+  username = '';
+  password = '';
+  successMessage = '';
+  errorMessage = '';
+
+  constructor(private http: HttpClient, private router: Router) {}
+
+  onRegister() {
+    const newUser = { username: this.username, password: this.password };
+
+    // Cambiado de 'http://localhost:3000/register' a 'http://localhost:3000/api/register'
+    this.http.post<any>('http://localhost:3000/api/register', newUser).subscribe({
+      next: (response) => {
+        this.successMessage = '¡Cuenta creada con éxito! Redirigiendo...';
+        setTimeout(() => {
+          this.router.navigate(['/login']);
+        }, 1500);
+      },
+      error: (err) => {
+        this.errorMessage = 'Error al registrar el usuario';
+        console.error(err);
+      }
+    });
+  }
+}
