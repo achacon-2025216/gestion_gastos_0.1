daeeb761297876b1ad3af517d71796382d39f719
achacon-2025216 <achacon-2025216@kinal.edu.gt>
2026-08-13 22:16:18 -0600

    feat: Se cambio la estructura del authRoute para los roles

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,8 +1,8 @@
-
 import { Router } from 'express';
-import { login } from '../controllers/authController.js';
+import { registerUser } from '../controllers/authController.js';
+
 const router = Router();
 
-router.post('/login', login);
+router.post('/register', registerUser);
 
 export default router;
