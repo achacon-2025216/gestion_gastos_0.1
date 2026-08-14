e8cb4ab2c5662672fd63a2257340a2d9d8c9b773
achacon-2025216 <achacon-2025216@kinal.edu.gt>
2026-08-14 00:51:41 -0600

    feat: Se arreglo la ruta de authRoutes.ts

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
-import { registerUser } from '../controllers/authController.js';
+import { registerUser, loginUser } from '../controllers/authController.js';
 
 const router = Router();
 
+// Rutas de autenticación
 router.post('/register', registerUser);
+router.post('/login', loginUser); // <--- Esta es la ruta que causaba el error 404
 
 export default router;
