324a55a9b58ae89df7d5e0482c404817b9dde148
achacon-2025216 <achacon-2025216@kinal.edu.gt>
2026-08-14 01:00:33 -0600

    feat: Se ha estado cambiando para mostrar las demas vistas

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './componentes/login/login';
 import { RegistroComponent } from './componentes/registro/registro';
+import { InicioGastos } from './componentes/inicio-gastos/inicio-gastos';
+import { authGuard } from './auth.guard'; // <--- Importación correcta
 
 export const routes: Routes = [
-  { path: '', component: LoginComponent },           // <--- Esto hace que al abrir la app cargue el LOGIN primero
-  { path: 'registro', component: RegistroComponent }, // <--- Esto carga el registro cuando haces clic
-  { path: '**', redirectTo: '' }
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'registro', component: RegistroComponent },
+  { path: 'inicio-gastos', component: InicioGastos, canActivate: [authGuard] }, // <--- Protegido
+  { path: '**', redirectTo: 'login' }
 ];
