87a518f0804f8d46083c79cf78a6d2436c109ad3
achacon-2025216 <achacon-2025216@kinal.edu.gt>
2026-08-13 22:15:07 -0600

    feat: Se cambio la estrucutura del authController para los roles

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -1,12 +1,31 @@
 import type { Request, Response } from 'express';
+import { PrismaClient } from '../generated/prisma/index.js';
+import { PrismaPg } from '@prisma/adapter-pg';
+import pg from 'pg';
 
-export const login = (req: Request, res: Response) => {
-    const { email, password } = req.body;
+const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL });
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
 
-    // Aquí iría tu lógica de validación con la base de datos
-    if (email === "usuario@ejemplo.com" && password === "123456") {
-        return res.status(200).json({ message: "Inicio de sesión exitoso" });
-    }
+export const registerUser = async (req: Request, res: Response) => {
+  const { username, password } = req.body;
 
-    return res.status(401).json({ message: "Credenciales incorrectas" });
+  const role = username.toLowerCase() === 'admin' ? 'admin' : 'user';
+
+  try {
+    const newUser = await prisma.user.create({
+      data: {
+        username,
+        password,
+        role,
+      },
+    });
+
+    return res.status(201).json({
+      message: `Usuario registrado exitosamente como ${role}`,
+      user: newUser,
+    });
+  } catch (error) {
+    return res.status(500).json({ error: 'Error al registrar el usuario' });
+  }
 };
