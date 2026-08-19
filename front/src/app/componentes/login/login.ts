1b79ab4756b49c7801a9002a116a7bc2497f8ef7
achacon-2025216 <achacon-2025216@kinal.edu.gt>
2026-08-19 16:32:37 -0600

    feat: Se cambio el css y html del login y registro

--- front/src/app/componentes/login/login.ts
+++ front/src/app/componentes/login/login.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Router, RouterLink, ActivatedRoute } from '@angular/router';
-import { AuthService } from '../../services/auth'; // ajusta la ruta según donde esté login.ts
+import { AuthService } from '../../services/auth'; 
 
 @Component({
   selector: 'app-login',
@@ -17,6 +17,9 @@ export class LoginComponent implements OnInit {
   errorMessage = '';
   sessionExpired = false;
 
+  // Declaramos la variable para el logo
+  logoSrc = 'img/logo_check.png'; 
+
   constructor(
     private authService: AuthService,
     private router: Router,
@@ -24,7 +27,6 @@ export class LoginComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    // Detecta si llegamos aquí por logout automático (?expired=1)
     this.sessionExpired = this.route.snapshot.queryParamMap.get('expired') === '1';
   }
 
