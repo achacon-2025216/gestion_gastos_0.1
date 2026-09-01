1958cc8495f42a5776859d223c3ec9a9006dcdfa
achacon-2025216 <achacon-2025216@kinal.edu.gt>
2026-08-31 22:28:46 -0600

    feat: Se cambio el gastos.ts

--- front/src/app/componentes/gastos/gastos.ts
+++ front/src/app/componentes/gastos/gastos.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-gastos',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterLink],
   templateUrl: './gastos.html',
   styleUrl: './gastos.css',
 })
