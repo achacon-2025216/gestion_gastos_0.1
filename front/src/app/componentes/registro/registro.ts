f2d0016fd7ba251ea2b00041e02d27386e9aaf35
achacon-2025216 <achacon-2025216@kinal.edu.gt>
2026-08-31 23:40:33 -0600

    feat: Se cambio el html y css para agregar el boton de google y el ts para implemental la logica

--- front/src/app/componentes/registro/registro.ts
+++ front/src/app/componentes/registro/registro.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+declare var google: any;
 
 @Component({
   selector: 'app-registro',
@@ -12,13 +14,45 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
   styleUrl: './registro.css'
 })
 
-export class RegistroComponent {
+export class RegistroComponent implements AfterViewInit {
   username = '';
   password = '';
   successMessage = '';
   errorMessage = '';
 
+  @ViewChild('googleBtn', { static: false }) googleBtn!: ElementRef;
+
   constructor(private http: HttpClient, private router: Router) {}
+
+  ngAfterViewInit(): void {
+    this.initGoogleSignIn();
+  }
+
+  initGoogleSignIn(): void {
+    if (typeof google !== 'undefined') {
+      google.accounts.id.initialize({
+        client_id: '463867676917-g8hga9ugqt9um24hpkoakhrl7jjhbs.apps.googleusercontent.com',
+        callback: (response: any) => this.handleGoogleCredentialResponse(response)
+      });
+
+      if (this.googleBtn && this.googleBtn.nativeElement) {
+        google.accounts.id.renderButton(
+          this.googleBtn.nativeElement,
+          { 
+            theme: 'outline', 
+            size: 'large', 
+            shape: 'pill', 
+            width: '100%'  
+          }
+        );
+      }
+    }
+  }
+
+  handleGoogleCredentialResponse(response: any): void {
+    console.log('Token JWT de registro con Google obtenido:', response.credential);
+    // Aquí puedes agregar la lógica para enviar el token a tu backend si lo requieres
+  }
 
   onRegister() {
     const newUser = { username: this.username, password: this.password };
