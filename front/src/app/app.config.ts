55d6f7adabfe3f8a21ee19f522e23667a7472db2
achacon-2025216 <achacon-2025216@kinal.edu.gt>
2026-08-31 23:41:41 -0600

    feat: Se cambiaron la logica para implementar la api de google

--- front/src/app/app.config.ts
+++ front/src/app/app.config.ts
@@ -1,14 +1,35 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideZonelessChangeDetection, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
-
+import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
-import { authInterceptor } from './interceptors/auth-interceptor';
+import { 
+  GoogleLoginProvider, 
+  SocialAuthServiceConfig, 
+  SocialLoginModule 
+} from '@abacritt/angularx-social-login';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
+    provideZonelessChangeDetection(),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([authInterceptor]))
+    provideHttpClient(),
+    importProvidersFrom(SocialLoginModule), // <-- Esto registra el SocialAuthService y su configuración correctamente
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: false,
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider(
+              '463867676917-g8hga9ugqt9um24hpkoakhrl7jjhbs.apps.googleusercontent.com'
+            )
+          }
+        ],
+        onError: (err) => {
+          console.error(err);
+        }
+      } as SocialAuthServiceConfig
+    }
   ]
 };
