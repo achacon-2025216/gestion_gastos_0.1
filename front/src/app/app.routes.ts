dc25fc97ca54f120061e322d2f5120dea4f2e0cd
achacon-2025216 <achacon-2025216@kinal.edu.gt>
2026-08-31 22:16:29 -0600

    feat: Se cambio el routes.ts, para el

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './componentes/login/login';
 import { RegistroComponent } from './componentes/registro/registro';
 import { InicioGastos } from './componentes/inicio-gastos/inicio-gastos';
+import { Gastos } from './componentes/gastos/gastos';
 import { authGuard } from './auth.guard';
 
 export const routes: Routes = [
@@ -9,5 +10,6 @@ export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'registro', component: RegistroComponent },
   { path: 'inicio-gastos', component: InicioGastos, canActivate: [authGuard] },
+  { path: 'gastos', component: Gastos, canActivate: [authGuard] },
   { path: '**', redirectTo: 'login' }
 ];
