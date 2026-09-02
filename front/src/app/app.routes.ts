e89931897ca2946b2134ce4c8eb5e1db45429b6b
achacon-2025216 <achacon-2025216@kinal.edu.gt>
2026-09-01 22:25:14 -0600

    feat: Se cambio el menu dentro tenia gestion de gastos a ingresos

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './componentes/login/login';
 import { RegistroComponent } from './componentes/registro/registro';
 import { InicioGastos } from './componentes/inicio-gastos/inicio-gastos';
-import { Gastos } from './componentes/gastos/gastos';
+import { Ingresos } from './componentes/ingresos/ingresos';
 import { authGuard } from './auth.guard';
 
 export const routes: Routes = [
@@ -10,6 +10,6 @@ export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'registro', component: RegistroComponent },
   { path: 'inicio-gastos', component: InicioGastos, canActivate: [authGuard] },
-  { path: 'gastos', component: Gastos, canActivate: [authGuard] },
+  { path: 'gastos', component: Ingresos, canActivate: [authGuard] },
   { path: '**', redirectTo: 'login' }
 ];
